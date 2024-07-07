0308f969ae45aaab0440e19b77a5e2ca1eabda91
David <dj@dj.je>
2024-07-06 21:11:41 -0500

    Change PlaySoundLocally to SoundMode enum, fix breaking when sound URI's are empty hopefully

--- SquishPanels.cs
+++ SquishPanels.cs
@@ -11,18 +11,20 @@ public class SquishPanels : ResoniteMod
 {
     public override string Author => "Cyro";
     public override string Name => "SquishPanels";
-    public override string Version => "3.0.0";
+    public override string Version => "3.1.0";
 
     public static float DopplerLevel = 0.0f;
     public static AudioDistanceSpace DistSpace = AudioDistanceSpace.Global;
     public static float TweenSpeed = 0.22f;
     private static ModConfiguration? Config;
 
+    private enum SoundModeEnum { Global, Local, NoSound }
+
     [AutoRegisterConfigKey]
     private static ModConfigurationKey<bool> Enabled = new ModConfigurationKey<bool>("Enabled", "Enables or disables the mod", () => true);
 
     [AutoRegisterConfigKey]
-    private static ModConfigurationKey<bool> PlaySoundLocally = new ModConfigurationKey<bool>("PlaySoundLocally", "Makes the sound local for the mod user if true", () => false);
+    private static ModConfigurationKey<SoundModeEnum> SoundMode = new ModConfigurationKey<SoundModeEnum>("SoundMode", "Dictates how the open/close sound is played", () => SoundModeEnum.Global);
 
     [AutoRegisterConfigKey]
     private static ModConfigurationKey<string> OpenSoundURL = new ModConfigurationKey<string>("OpenSoundURL", "The URL of the sound to play when the panel opens", () => "resdb:///bbdf36b8f036a5c30f7019d68c1fbdd4032bb1d4c9403bcb926bb21cd0ca3c1a.wav");
@@ -38,10 +40,10 @@ public class SquishPanels : ResoniteMod
     public override void OnEngineInit()
     {
         Config = GetConfiguration();
-        
+
         if (Config == null)
             throw new NullReferenceException("Config is null");
-        
+
         Config.Save(true);
         Harmony harmony = new Harmony("net.Cyro.SquishPanels");
         harmony.PatchAll();
@@ -51,63 +53,65 @@ public class SquishPanels : ResoniteMod
     public static class LegacyPanel_OnAttach_Patch
     {
 
-        private static bool ShouldPlayLocally()
-        {
-            string? description = Engine.Current.WorldManager.FocusedWorld.Description;
-            return description != null && description.Contains("##SquishPanels.ForceLocal##") || Config!.GetValue<bool>(PlaySoundLocally);
-        }
+        private static bool ShouldPlayLocally() => Config!.GetValue(SoundMode) != SoundModeEnum.Global || Engine.Current.WorldManager.FocusedWorld.Description.Contains("##SquishPanels.ForceLocal##");
+
         public static void PlayOpenSound(LegacyPanel __instance)
         {
-            StaticAudioClip clip = __instance.World.GetSharedComponentOrCreate<StaticAudioClip>(Config!.GetValue<string>(OpenSoundURL), a => a.URL.Value = new Uri(Config!.GetValue<string>(OpenSoundURL)));
+            if (Config!.GetValue(SoundMode) == SoundModeEnum.NoSound || !Config!.TryGetValue(OpenSoundURL, out var url)) return;
+            StaticAudioClip clip = __instance.World.GetSharedComponentOrCreate<StaticAudioClip>("##SquishPanels.OpenSoundURL##", a => a.URL.Value = new Uri(url));
             AudioOutput audio = __instance.World.PlayOneShot(__instance.Slot.GlobalPosition, clip, 1f, true, 1f, __instance.Slot, AudioDistanceSpace.Local, ShouldPlayLocally());
-            // UniLog.Log("Playing Open Sound " + (ShouldPlayLocally() ? "Locally" : "Globally").ToString());
+            Debug("Playing Open Sound " + (ShouldPlayLocally() ? "Locally" : "Globally").ToString());
             audio.DopplerLevel.Value = DopplerLevel;
             audio.DistanceSpace.Value = DistSpace;
         }
 
         public static void PlayCloseSound(LegacyPanel __instance)
         {
-            StaticAudioClip clip = __instance.World.GetSharedComponentOrCreate<StaticAudioClip>(Config!.GetValue<string>(CloseSoundURL), a => a.URL.Value = new Uri(Config!.GetValue<string>(CloseSoundURL)));
+            if (Config!.GetValue(SoundMode) == SoundModeEnum.NoSound || !Config!.TryGetValue(CloseSoundURL, out var url)) return;
+            StaticAudioClip clip = __instance.World.GetSharedComponentOrCreate<StaticAudioClip>("##SquishPanels.CloseSoundURL##", a => a.URL.Value = new Uri(url));
             AudioOutput audio = __instance.World.PlayOneShot(__instance.Slot.GlobalPosition, clip, 1f, true, 1f, __instance.Slot, AudioDistanceSpace.Local, ShouldPlayLocally());
-            // UniLog.Log("Playing Close Sound " + (ShouldPlayLocally() ? "Locally" : "Globally").ToString());
+            Debug("Playing Close Sound " + (ShouldPlayLocally() ? "Locally" : "Globally").ToString());
             audio.DopplerLevel.Value = DopplerLevel;
             audio.DistanceSpace.Value = DistSpace;
         }
 
         public static void Postfix(LegacyPanel __instance)
         {
-            if (!Config!.GetValue<bool>(Enabled))
+            if (!Config!.GetValue(Enabled))
                 return;
-            
+
             float3 Orig = __instance.Slot.LocalScale;
 
             SyncListElementsEvent<SyncRef<IBounded>>? CanvasListener = null;
-            
-            CanvasListener = (SyncElementList<SyncRef<IBounded>> list, int StartIndex, int count) => {
-                __instance.RunInUpdates(0, () => {
+
+            CanvasListener = (SyncElementList<SyncRef<IBounded>> list, int StartIndex, int count) =>
+            {
+                __instance.RunInUpdates(0, () =>
+                {
                     if (list[StartIndex] == null)
                         return;
-                    
+
                     if (list[StartIndex].Target is Canvas)
                     {
                         Canvas? c = list[StartIndex].Target as Canvas;
-                        
+
                         if (c != null)
                         {
                             float2 OrigSize = c.Size.Value;
                             PlayOpenSound(__instance);
-                            c.Size.TweenFrom(new float2(OrigSize.x, 0f), Config!.GetValue<float>(PanelOpenSpeed));
+                            c.Size.TweenFrom(new float2(OrigSize.x, 0f), Config!.GetValue(PanelOpenSpeed));
                         }
                     }
                     __instance.WhiteList.ElementsAdded -= CanvasListener;
                 });
             };
-            __instance.RunInUpdates(0, () => {
+            __instance.RunInUpdates(0, () =>
+            {
                 if (__instance.WhiteList.Count < 1)
                 {
                     float3 OrigSize = __instance.Slot.LocalScale;
                     PlayOpenSound(__instance);
-                    __instance.Slot.Scale_Field.TweenFrom(new float3(OrigSize.x, 0f, OrigSize.z), Config!.GetValue<float>(PanelOpenSpeed));
+                    __instance.Slot.Scale_Field.TweenFrom(new float3(OrigSize.x, 0f, OrigSize.z), Config!.GetValue(PanelOpenSpeed));
                     __instance.WhiteList.ElementsAdded -= CanvasListener;
                 }
             });
@@ -131,10 +135,11 @@ public class SquishPanels : ResoniteMod
     {
         public static bool Prefix(LegacyPanel __instance, LegacyPanel.TitleButton button)
         {
-            if(!Config!.GetValue<bool>(Enabled))
+            if (!Config!.GetValue(Enabled))
                 return true;
-            
-            Action OnTweenDoneAction = () => {
+
+            Action OnTweenDoneAction = () =>
+            {
                 if (__instance.World.IsUserspace())
                     __instance.Slot.Destroy();
                 else
@@ -145,7 +150,7 @@ public class SquishPanels : ResoniteMod
             {
                 float3 OrigSize = __instance.Slot.LocalScale;
                 LegacyPanel_OnAttach_Patch.PlayCloseSound(__instance);
-                __instance.Slot.Scale_Field.TweenTo(new float3(OrigSize.x, 0f, OrigSize.z), Config!.GetValue<float>(PanelCloseSpeed), default, null, OnTweenDoneAction);
+                __instance.Slot.Scale_Field.TweenTo(new float3(OrigSize.x, 0f, OrigSize.z), Config!.GetValue(PanelCloseSpeed), default, null, OnTweenDoneAction);
                 return false;
             }
 
@@ -158,9 +163,9 @@ public class SquishPanels : ResoniteMod
 
                 if (c == null)
                     return true;
-                
+
                 LegacyPanel_OnAttach_Patch.PlayCloseSound(__instance);
-                c.Size.TweenTo(new float2(c.Size.Value.x, 0f), Config!.GetValue<float>(PanelCloseSpeed), default, null, OnTweenDoneAction);
+                c.Size.TweenTo(new float2(c.Size.Value.x, 0f), Config!.GetValue(PanelCloseSpeed), default, null, OnTweenDoneAction);
                 return false;
             }
             return true;
